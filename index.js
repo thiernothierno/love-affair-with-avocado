94c32556f99b7d971533fbc27f05fde7e473b6d6
Thierno dicko <thiernomougnal@gmail.com>
2026-01-29 14:27:22 -0700

    new commit

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
-app.get("/about", (req, res) =>{
+app.get("/about", (req, res) =>{ 
     res.render("about.ejs")
 })
 
@@ -21,6 +22,8 @@ app.get("/contact", (req, res) =>{
 app.get("/post", (req, res) => {
     res.render("post.ejs")
 })
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
