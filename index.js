839d2d10c96dd73e2ce939b37f9ff1205d754422
Thierno dicko <thiernomougnal@gmail.com>
2026-01-30 11:48:55 -0700

    new update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express"
+import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({ extended: true }));  
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs" )
 })
 
 app.get("/about", (req, res) =>{ 
@@ -19,10 +20,31 @@ app.get("/contact", (req, res) =>{
 })
 
 
-app.get("/post", (req, res) => {
+app.get("/comment", (req, res) => {
     res.render("post.ejs")
 })
 
+app.get("/all-posts", (req, res) =>{
+    res.render("post.ejs")
+})
+
+app.post("/create-post", (req, res) => {
+    const data = {
+        name : req.body['name'],
+        email : req.body['email'],
+        favorite_fruit : req.body['favorite-fruit'],
+    }
+    res.render("all-post.ejs", {Posts : data})
+})
+
+app.put("/update-post", (req, res) =>{
+    const data = req.body
+    res.render("all-post.ejs", data)
+})
+
+app.delete("/delete-post", (req, res) => {
+
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
