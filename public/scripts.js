38062822a56ee4a991d9b016c8500bc467b8a715
Thierno dicko <thrndicko@gmail.com>
2026-02-19 18:41:27 -0700

    update

--- public/scripts.js
+++ public/scripts.js
@@ -52,37 +52,37 @@
         "Balanced health feels achievable with avocado on the menu."
         ]
 
-        const fruits_images = [
-            "https://th.bing.com/th/id/OIP.DzzBtp9wRuY1VocmOurZ7gHaJE?w=152&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.XprgHSCfPgHBpfQBBfAWqwHaFb?w=262&h=192&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.k1bcs4IVQF6QM2TSpfpw9QHaFi?w=250&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.qNwRfXgaZn0IFZnDQVjTXwHaDj?w=326&h=168&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.AzP3m-pF7lOgZcFGyw_k9gHaEo?w=280&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.cx-n_ANc4mMP7mmz6AN1_gHaE7?w=242&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.YgJ35_c86TUwj10CbwIAXQHaEL?w=283&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.qzKvCvK6VrjE0K3U1p_iogHaFX?w=241&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://m.media-amazon.com/images/I/71HZjvrnUcL._AC_SY300_SX300_QL70_FMwebp_.jpg",
-            "https://th.bing.com/th/id/OIP.zkUqUyC5bDtTPUpBAzsPyAHaE5?w=284&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.O2USwwdIpEbA5gv31ZqxSQHaEV?w=317&h=186&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.H6jz9BWMtvOpznbge6Ys_gHaE8?w=290&h=193&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.GjILLa5IQponwjalsDe36wHaEo?w=287&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://tse2.mm.bing.net/th/id/OIP.iGqgNZZKkfumi0svy4M7vgHaFj?rs=1&pid=ImgDetMain&o=7&rm=3",
-            "https://th.bing.com/th/id/OIP.pYjCn3Zm4bTxY3-Fx8DhbQHaE6?w=238&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP._Gs8aLMrPB-7shk2EeY2_gHaEK?w=248&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.bkWhwJ-Zhedil9s4UBGh1QHaFK?w=249&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.Fue1-3svXSbY-Hfhef7FlQHaE5?w=271&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.Z0dP6UuG2P3rvU3a3iQHlQHaFD?w=265&h=181&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.6iHie1tkQYGLAFL5IsC6GAHaFv?w=223&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.j4bywQ-VSxVQplIpy3OBPwHaE8?w=230&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.ZpjlJc6Qy-Eba6Y1HK4cmwHaFs?w=244&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.1gAOFd7GO2iGLQuPNNhkqwHaEo?w=281&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3", 
-            "https://th.bing.com/th/id/OIP.zjzFRpT7Kwr3rMzoOLtwiwHaFj?w=249&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.DkBTTbs14YVScQ6GiqaXbwHaEJ?w=289&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.keRlXXxjNvzdBjIp_HNF6AHaFO?w=209&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.mZDhcV6LTVfAJ2aKcL_KRQHaFS?w=222&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
-            "https://th.bing.com/th/id/OIP.pITzY9neU8xyuYE62rLvVAHaEv?w=273&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3"
+        const fruits_images = {
+            Banana : "https://th.bing.com/th/id/OIP.DzzBtp9wRuY1VocmOurZ7gHaJE?w=152&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Apple : "https://th.bing.com/th/id/OIP.XprgHSCfPgHBpfQBBfAWqwHaFb?w=262&h=192&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Orange : "https://th.bing.com/th/id/OIP.k1bcs4IVQF6QM2TSpfpw9QHaFi?w=250&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Mango : "https://th.bing.com/th/id/OIP.qNwRfXgaZn0IFZnDQVjTXwHaDj?w=326&h=168&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Grape : "https://th.bing.com/th/id/OIP.AzP3m-pF7lOgZcFGyw_k9gHaEo?w=280&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Watermelon : "https://th.bing.com/th/id/OIP.cx-n_ANc4mMP7mmz6AN1_gHaE7?w=242&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Pineapple: "https://th.bing.com/th/id/OIP.YgJ35_c86TUwj10CbwIAXQHaEL?w=283&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Papaya : "https://th.bing.com/th/id/OIP.qzKvCvK6VrjE0K3U1p_iogHaFX?w=241&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Strawbery: "https://m.media-amazon.com/images/I/71HZjvrnUcL._AC_SY300_SX300_QL70_FMwebp_.jpg",
+            Lemon : "https://th.bing.com/th/id/OIP.zkUqUyC5bDtTPUpBAzsPyAHaE5?w=284&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Lime : "https://th.bing.com/th/id/OIP.O2USwwdIpEbA5gv31ZqxSQHaEV?w=317&h=186&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Pear : "https://th.bing.com/th/id/OIP.H6jz9BWMtvOpznbge6Ys_gHaE8?w=290&h=193&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Peach : "https://th.bing.com/th/id/OIP.GjILLa5IQponwjalsDe36wHaEo?w=287&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Cherry : "https://tse2.mm.bing.net/th/id/OIP.iGqgNZZKkfumi0svy4M7vgHaFj?rs=1&pid=ImgDetMain&o=7&rm=3",
+            Plum : "https://th.bing.com/th/id/OIP.pYjCn3Zm4bTxY3-Fx8DhbQHaE6?w=238&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Kiwi : "https://th.bing.com/th/id/OIP._Gs8aLMrPB-7shk2EeY2_gHaEK?w=248&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Blueberry : "https://th.bing.com/th/id/OIP.bkWhwJ-Zhedil9s4UBGh1QHaFK?w=249&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Raspberry : "https://th.bing.com/th/id/OIP.Fue1-3svXSbY-Hfhef7FlQHaE5?w=271&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Blackberry : "https://th.bing.com/th/id/OIP.Z0dP6UuG2P3rvU3a3iQHlQHaFD?w=265&h=181&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Coconut : "https://th.bing.com/th/id/OIP.6iHie1tkQYGLAFL5IsC6GAHaFv?w=223&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Pomegranate : "https://th.bing.com/th/id/OIP.j4bywQ-VSxVQplIpy3OBPwHaE8?w=230&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Guave : "https://th.bing.com/th/id/OIP.ZpjlJc6Qy-Eba6Y1HK4cmwHaFs?w=244&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Fig : "https://th.bing.com/th/id/OIP.1gAOFd7GO2iGLQuPNNhkqwHaEo?w=281&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3", 
+            Date : "https://th.bing.com/th/id/OIP.zjzFRpT7Kwr3rMzoOLtwiwHaFj?w=249&h=187&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Avocado : "https://th.bing.com/th/id/OIP.DkBTTbs14YVScQ6GiqaXbwHaEJ?w=289&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Cantaloupe : "https://th.bing.com/th/id/OIP.keRlXXxjNvzdBjIp_HNF6AHaFO?w=209&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Honeydrew : "https://th.bing.com/th/id/OIP.mZDhcV6LTVfAJ2aKcL_KRQHaFS?w=222&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
+            Grapefruit : "https://th.bing.com/th/id/OIP.pITzY9neU8xyuYE62rLvVAHaEv?w=273&h=180&c=7&r=0&o=7&dpr=1.1&pid=1.7&rm=3",
 
-        ]
+        }
 
         // function to get a random quote from the list of quotes.
     function getQuote(){
@@ -94,15 +94,19 @@
     // Display every 5 seconds a new quote from the list of quotes. 
     setInterval(getQuote, 5000)
 
-    
+
     // function to get an image from the list of images.
     function getImage(){
-        const randomImage = fruits_images[Math.floor(Math.random() * fruits_images.length )]
-        document.getElementById("fruit").src = randomImage
+        const key = Object.keys(fruits_images)
+        const randomImage = key[Math.floor(Math.random() * key.length )];
+        document.getElementById("fruit-img").src = fruits_images[randomImage];
+        document.getElementById("fruit-name").innerHTML = randomImage;
 
     }
     getImage()
-     setInterval(getImage, 5000)
+    setInterval(getImage, 5000)
+
+    
 
     
    
